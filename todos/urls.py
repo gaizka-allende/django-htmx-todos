c92a6bc029eba13bde53695b13435bcedc9daf97
Gaizka Allende <gaizka@allende.org>
2025-01-29 13:59:36 +0000

    added user registration

--- todos/urls.py
+++ todos/urls.py
@@ -1,12 +1,13 @@
 # todos/urls.py
 from django.urls import path
 
-from todos.views import index, login, logout_view, admin
+from todos.views import index, login, logout_view, register, admin
 
 
 urlpatterns = [
     path('', index),
     path('login', login),
     path('logout', logout_view),
+    path('register', register),
     path('admin', admin)
 ]
--- todos/views.py
+++ todos/views.py
@@ -4,35 +4,46 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout
 from todos.models import Logins
-from bcrypt import checkpw
+from bcrypt import checkpw, hashpw, gensalt
 
 def index(request):
-  html = f'''
-  <html>
-      <body>
-          <h1>Todos</h1>
-      </body>
-  </html>
-  '''
-  return HttpResponse(html)
+  return request(request, 'index.html')
 
 @csrf_exempt 
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
+        password = request.POST.get('password')
         user = Logins.objects.filter(username=username).first()
         if user is None:
           return HttpResponse('User not found', status=404)
-        if not checkpw(request.POST.get('password').encode(), user.password.encode()):
+        if not checkpw(password.encode(), user.password.encode()):
           return HttpResponse('Password incorrect', status=401)
         request.session['username'] = username
-        return render(request, 'loggedIn.html', {username})
+        return render(request, 'loggedIn.html', {'username': username})
     elif request.method == 'GET':
       return render(request, 'login.html')
   
 def logout_view(request):
     logout(request)
     return render(request, 'loggedOut.html')
+
+@csrf_exempt 
+def register(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    reEnterPassword = request.POST.get('reEnterPassword')
+    if password != reEnterPassword:
+      return HttpResponse('Passwords do not match', status=400)
+    if Logins.objects.filter(username=username).first() is not None:
+      return HttpResponse('User already exists', status=400)
+    Logins.objects.create(username=username, password=hashpw(password.encode(), gensalt()).decode())
+    request.session['username'] = username
+    return render(request, 'index.html')
+  else:
+    return render(request, 'register.html')
+
 
 def admin(request):
     return HttpResponse('<h1>Admin</h1>')
