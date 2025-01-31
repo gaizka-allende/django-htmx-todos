bcc8995d612ba5d60a109acaff01b83862403618
Gaizka Allende <gaizka@allende.org>
2025-01-31 12:36:04 +0000

    use django's auth models instead of Logins

--- todos/models.py
+++ todos/models.py
@@ -6,16 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
-class Logins(models.Model):
-    username = models.CharField(blank=True, null=True, max_length=255)
-    password = models.CharField(blank=True, null=True, max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'logins'
-
+from django.contrib.auth.models import User
 
 class Suggestions(models.Model):
     title = models.CharField(blank=True, null=True, max_length=255)
@@ -26,7 +17,7 @@ class Suggestions(models.Model):
 
 
 class Todos(models.Model):
-    user = models.ForeignKey(Logins, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     title = models.CharField(blank=True, null=True, max_length=255)
     completed = models.BooleanField(blank=True, null=True)
     created_modified = models.TextField(blank=True, null=True)
--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,11 @@
 # todos/views.py
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout
-from todos.models import Logins
-from bcrypt import checkpw, hashpw, gensalt
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 
 def index(request):
   return render(request, 'index.html')
@@ -15,11 +15,9 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = Logins.objects.filter(username=username).first()
+        user = authenticate(username=username, password=password)
         if user is None:
-          return HttpResponse('User not found', status=404)
-        if not checkpw(password.encode(), user.password.encode()):
-          return HttpResponse('Password incorrect', status=401)
+          return HttpResponse('Invalid username or password', status=404)
         request.session['username'] = username
         response = render(request, 'index.html')
         response['HX-Replace-Url'] = '/'
@@ -39,11 +37,15 @@ def register(request):
     reEnterPassword = request.POST.get('reEnterPassword')
     if password != reEnterPassword:
       return HttpResponse('Passwords do not match', status=400)
-    if Logins.objects.filter(username=username).first() is not None:
+    if User.objects.filter(username=username).exists():
       return HttpResponse('User already exists', status=400)
-    Logins.objects.create(username=username, password=hashpw(password.encode(), gensalt()).decode())
+    user = User.objects.create_user(username=username, password=password)
+    if user is None:
+      return HttpResponse('Error creating user', status=500)
     request.session['username'] = username
-    return render(request, 'index.html')
+    response = render(request, 'index.html')
+    response['HX-Replace-Url'] = '/'
+    return response
   else:
     return render(request, 'register.html')
 
