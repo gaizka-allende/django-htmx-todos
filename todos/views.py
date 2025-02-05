22ef741066b03066329f6549f6ca60cffc9b9d79
Gaizka Allende <gaizka@allende.org>
2025-02-05 14:35:15 +0000

    add list of todos

--- todos/views.py
+++ todos/views.py
@@ -6,10 +6,19 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from todos.models import Todos
+from todos.utils.date import formatTodoDate
 
 @login_required(login_url='/login')
 def index(request):
-  return render(request, 'index.html')
+    todos = Todos.objects.filter(user=request.user)
+    print(formatTodoDate(todos[0].created_modified))
+    return render(request, 'index.html', {'todos': map(lambda todo: {
+      'id': todo.id,
+      'title': todo.title,
+      'completed': todo.completed,
+      'created_modified': formatTodoDate(todo.created_modified)
+    }, todos)})
 
 @csrf_exempt 
 def login_view(request):
